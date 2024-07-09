23caa6cd29c8ad3365838d6691d6adae7d91d06b
ismailelsheanwy <ismailelshenawy777@gmail.com>
2024-07-10 02:02:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <h1>hello in task 3 in php</h1>
+<div class="form">
+        <form action="./logging.php" method="post">
+      <div class="inputEmail">
+          <label for="mail">Mail</label>
+          <input type="email" name="email" id="mail">
+      </div>
+    <div class="inputPassword">
+            <label for="password">password</label>
+            <input type="password" name="password" id="mail" >
+    </div>
+        <button type="submit">log in</button>
+        </form>
+</div>
+    
+</body>
+</html>
